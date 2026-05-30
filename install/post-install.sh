cceae647e03341b48472c39c39fb08736dea870d
Grang404 <chunter404@proton.me>
2026-05-30 22:29:31 +0100

    fix(post-install): typo and add wallpaper init

--- install/post-install.sh
+++ install/post-install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-exec >"$GROOBDIR/logs/post-install.log" 2>&1
+exec >"$GROOB_DIR/logs/post-install.log" 2>&1
 
 hyprpm update
 yes | hyprpm add https://github.com/outfoxxed/hy3
 
 # notify-send "groobarchy setup complete"
+
+"$GROOB_DIR/bin/groob-randompaper"
 
 sed -i '/groob-boot-setup/d' "$HOME/.config/hypr/hyprland.conf"
 rm -f /etc/sudoers.d/groob-boot-setup
