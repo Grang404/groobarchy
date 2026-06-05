14a7d9a4baf72f21c17d58688dcea37d87c55e40
Grang404 <chunter404@proton.me>
2026-06-05 03:35:57 +0100

    fix post install setup

--- install/99-post-install-setup.sh
+++ install/99-post-install-setup.sh
@@ -7,10 +7,10 @@ chmod +x "$BOOT_SCRIPT"
 echo "$SUDO_USER ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/groob-post-install
 chmod 440 /etc/sudoers.d/groob-post-install
 
-sed -i "s/^end)/\thl.exec_cmd(\"$BOOT_SCRIPT\")\nend)/" "$USER_HOME/.config/hypr/autostart.lua"
+awk -v cmd="$BOOT_SCRIPT" '/^end\)/{print "\thl.exec_cmd(\""cmd"\")"}1' "$DOTS/hypr/shared/autostart.lua" > /tmp/autostart.lua && mv /tmp/autostart.lua "$DOTS/hypr/shared/autostart.lua"
 
 print_success "Post install script set up"
 print_warning "Rebooting..."
 
-sleep 2
-reboot
+# sleep 2
+# reboot
