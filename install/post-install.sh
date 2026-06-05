8e631e8fc53cb734491ba711b9a9425864e612c7
Grang404 <chunter404@proton.me>
2026-06-05 03:42:41 +0100

    post install maybe not cooked now?

--- install/post-install.sh
+++ install/post-install.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 exec >"$GROOB_DIR/logs/post-install.log" 2>&1
 
+notify-send "Groobarchy" "Groobarchy post-setup starting..."
+
 hyprpm update
 yes | hyprpm add https://github.com/outfoxxed/hy3
+hyprpm enable
 
-notify-send "groobarchy setup complete"
+awk -v cmd="$BOOT_SCRIPT" '!(/hl\.exec_cmd/ && index($0, cmd))' "$DOTS/hypr/shared/autostart.lua" > /tmp/autostart.lua && mv /tmp/autostart.lua "$DOTS/hypr/shared/autostart.lua"
 
-"$GROOB_DIR/bin/groob-randompaper"
-
-sed -i "\|hl.exec_cmd(\"$BOOT_SCRIPT\")|d" "$HOME/.config/hypr/autostart.lua"
 sudo rm -f /etc/sudoers.d/groob-post-install
+
+
+notify-send "Groobarchy" "Groobarchy post-setup finished!"
+"$GROOB_DIR/bin/groob-randompaper"
