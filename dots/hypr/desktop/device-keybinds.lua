bfbabebcda103e051207ee3344030f046db4ae4b
Grang404 <chunter404@proton.me>
2026-06-22 07:10:33 +0100

    removed workspace 10 as it conflicts with other useful keybinds

--- dots/hypr/desktop/device-keybinds.lua
+++ dots/hypr/desktop/device-keybinds.lua
@@ -12,14 +12,12 @@ for i = 1, 5 do
 	hl.bind("ALT + SHIFT + " .. i, hl.dsp.window.move({ workspace = i + 5 }))
 end
 
-for i = 6, 10 do
-	local key = (i == 10) and "0" or tostring(i)
+for i = 6, 9 do
 	local ws = i + 5
-
-	hl.bind("CONTROL + " .. key, hl.dsp.focus({ workspace = ws }))
-	hl.bind("ALT + " .. key, hl.dsp.focus({ workspace = ws + 5 }))
-	hl.bind("CONTROL + SHIFT + " .. key, hl.dsp.window.move({ workspace = ws }))
-	hl.bind("ALT + SHIFT + " .. key, hl.dsp.window.move({ workspace = ws + 5 }))
+	hl.bind("CONTROL + " .. i, hl.dsp.focus({ workspace = ws }))
+	hl.bind("ALT + " .. i, hl.dsp.focus({ workspace = ws + 5 }))
+	hl.bind("CONTROL + SHIFT + " .. i, hl.dsp.window.move({ workspace = ws }))
+	hl.bind("ALT + SHIFT + " .. i, hl.dsp.window.move({ workspace = ws + 5 }))
 end
 
 hl.bind("SUPER + K", hl.dsp.focus({ workspace = "name:uwu" }))
