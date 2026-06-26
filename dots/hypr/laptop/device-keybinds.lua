9a7a298d76607a4a04674dad994186c106eaac5a
Grang404 <chunter404@proton.me>
2026-06-26 13:34:23 +0100

    laptop keybind/workspace changes

--- dots/hypr/laptop/device-keybinds.lua
+++ dots/hypr/laptop/device-keybinds.lua
@@ -1,7 +1,7 @@
 -- KEYBIND: CONTROL + 0-9 → Focus Workspace
 -- KEYBIND: CONTROL + SHIFT + 0-9 → Move Window to Workspace
 -- KEYBINDS_SKIP_START
-for i = 1, 10 do
+for i = 1, 9 do
 	local key = i % 10
 	hl.bind("CONTROL + " .. key, hl.dsp.focus({ workspace = i })) -- Move to i Workspace
 	hl.bind("CONTROL + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
