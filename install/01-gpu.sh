a742a3edd19544e94e4725a55661b922ad1d3270
Grang404 <chunter404@proton.me>
2026-08-30 23:37:31 +0100

    upgrades people upgrades

--- install/01-gpu.sh
+++ install/01-gpu.sh
@@ -13,7 +13,7 @@ install_gpu_pkgs() {
 
 case "$GPU" in
 amd) install_gpu_pkgs vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa ;;
-nvidia) install_gpu_pkgs nvidia nvidia-utils lib32-nvidia-utils ;;
+nvidia) install_gpu_pkgs nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings linux-zen-headers ;;
 intel) install_gpu_pkgs mesa lib32-mesa vulkan-intel lib32-vulkan-intel ;;
 *)
 	print_warning "Unknown GPU, defaulting to mesa"
